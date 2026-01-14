b51f9deceba623a50566c863c74a6725e2e0cabd
MGSimard <mgsimard.dev@gmail.com>
2026-01-14 14:22:38 -0500

    hero optimizations

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,13 +9,14 @@ function PageHome() {
     <main className="relative h-screen w-screen grid place-items-center">
       <video
         className="absolute inset-0 w-full h-full object-cover opacity-30 blur-sm"
-        src="/assets/media/hero.mp4"
-        poster="https://vda-global.lilithcdn.com/online_dragonfly_material/1658915262_0X520X4F0X4B0X5F0X6C0X750X6F0X6.jpg"
+        poster="/assets/media/hero.webp"
         autoPlay
         loop
         muted
-        playsInline
-      />
+        playsInline>
+        <source src="/assets/media/hero.webm" type="video/webm" />
+        <source src="/assets/media/hero.mp4" type="video/mp4" />
+      </video>
       <div className="z-1">
         <h1 className="text-7xl font-logo font-bold">
           Raider<span className="text-destructive animate-recording-pulse inline-block">.</span>Watch

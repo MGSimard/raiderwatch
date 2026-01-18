1e9d3d152adf7d6c3786971471bdb822e6abbdca
MGSimard <mgsimard.dev@gmail.com>
2026-01-18 01:42:19 -0500

    a

--- src/_components/admin/Header.tsx
+++ src/_components/admin/Header.tsx
@@ -1,8 +1,8 @@
 import { useLocation } from "@tanstack/react-router";
-import { XLogoIcon } from "@phosphor-icons/react";
+// import { XLogoIcon } from "@phosphor-icons/react";
 import { SidebarTrigger } from "@/_components/admin/ui/sidebar";
 import { Separator } from "@/_components/admin/ui/separator";
-import { Button } from "@/_components/ui/button";
+// import { Button } from "@/_components/ui/button";
 
 
 export function Header() {
@@ -14,7 +14,7 @@ export function Header() {
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="data-[orientation=vertical]:h-4 data-[orientation=vertical]:self-auto mr-1" />
         <h1 className="text-base font-medium capitalize">{pageName}</h1>
-        <div className="ml-auto flex items-center gap-2">
+        {/* <div className="ml-auto flex items-center gap-2">
           <Button
             variant="ghost"
             nativeButton={false}
@@ -22,7 +22,7 @@ export function Header() {
             size="icon">
             <XLogoIcon className="size-4" />
           </Button>
-        </div>
+        </div> */}
     </header>
   );
 }

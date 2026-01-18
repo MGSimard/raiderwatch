0fcb823a045be638559def9ea8f1203b680caa0e
MGSimard <mgsimard.dev@gmail.com>
2026-01-18 00:35:08 -0500

    Header adjustments

--- src/_components/admin/Header.tsx
+++ src/_components/admin/Header.tsx
@@ -1,15 +1,19 @@
+import { useLocation } from "@tanstack/react-router";
 import { XLogoIcon } from "@phosphor-icons/react";
 import { SidebarTrigger } from "@/_components/admin/ui/sidebar";
-import { Separator } from "@/_components/ui/separator";
+import { Separator } from "@/_components/admin/ui/separator";
 import { Button } from "@/_components/ui/button";
 
+
 export function Header() {
+  const location = useLocation();
+  const pageName = location.pathname.split("/").pop();
+
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+    <header className="shrink-0 flex items-center gap-2 px-4 lg:px-6 h-(--header-height) border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
-        <h1 className="text-base font-medium">Documents</h1>
+        <Separator orientation="vertical" className="data-[orientation=vertical]:h-4 data-[orientation=vertical]:self-auto mr-1" />
+        <h1 className="text-base font-medium capitalize">{pageName}</h1>
         <div className="ml-auto flex items-center gap-2">
           <Button
             variant="ghost"
@@ -19,7 +23,6 @@ export function Header() {
             <XLogoIcon className="size-4" />
           </Button>
         </div>
-      </div>
     </header>
   );
 }

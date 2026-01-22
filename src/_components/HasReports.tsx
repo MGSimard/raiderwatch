c4914437caafbcfb4397565f0b04ab8048bfd6ca
MGSimard <mgsimard.dev@gmail.com>
2026-01-22 03:41:17 -0500

    Couple adjustments to card, starting tests with list

--- src/_components/HasReports.tsx
+++ src/_components/HasReports.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from "@/_components/ui/button";
 import { Separator } from "@/_components/ui/separator";
 import type { getRaiderApprovedReports } from "@/_server/serverFunctions";
-import { ArrowLeftIcon, WarningIcon, UserSquareIcon } from "@phosphor-icons/react";
+import { ArrowLeftIcon, WarningIcon } from "@phosphor-icons/react";
 
 export function HasReports({
   embarkId,
@@ -14,44 +14,35 @@ export function HasReports({
   approvedReports: Awaited<ReturnType<typeof getRaiderApprovedReports>>;
 }) {
   return (
-    <section>
-      <Card className="w-full relative corner-brackets">
-        <div className="grid gap-4 expander">
-          <CardHeader>
-            <CardTitle disableGlow>
-              <h1 className="uppercase">{embarkId}</h1>
-              <p className="text-sm uppercase tracking-wider">FLAGGED</p>
-            </CardTitle>
+    <>
+      <section>
+        <Card className="w-full relative corner-brackets animated-height py-0">
+          <div className="grid gap-4 expander overflow-hidden py-4">
+            <CardHeader className="relative">
+              <svg
+                className="absolute -translate-y-1/2 top-1/2 right-0 h-[250%] text-primary/10"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 117.34 107.22"
+                stroke="none"
+                aria-hidden>
+                <path d="m116.89,101.98l-16.84-30.58-2.63,1.45-21.41-38.9,2.65-1.45L61.77,1.83c-1.35-2.43-4.86-2.43-6.2,0l-16.91,30.73,2.64,1.45-21.41,38.9-2.64-1.45L.44,101.98c-1.3,2.36.4,5.24,3.1,5.24h33.88v-2.75h42.5v2.75h33.86c2.7,0,4.4-2.88,3.1-5.24Zm-106.71-4.65l33.86-61.5,12.25-22.43c1.01-1.85,3.66-1.85,4.67,0l46.16,83.85c.98,1.77-.31,3.95-2.33,3.95l-92.27.08c-2.03,0-3.31-2.17-2.34-3.95Zm51.9-63.93v21.2c0,10.38-.73,19.97-.73,19.97h-5.41s-.73-9.59-.73-19.97v-21.2h6.86Zm-9.25,52.76c0-3.37,2.61-6.11,5.82-6.11s5.82,2.73,5.82,6.11-2.61,6.11-5.82,6.11-5.82-2.73-5.82-6.11Z" />
+              </svg>
+              <CardTitle disableGlow>
+                <h1 className="uppercase text-foreground truncate">{embarkId}</h1>
+                <p className="text-sm uppercase text-primary">FLAGGED</p>
+              </CardTitle>
+            </CardHeader>
+          </div>
+        </Card>
+      </section>
+      <section>
+        <Card className="w-full relative">
+          <CardHeader className="relative">
+            <CardDescription>Test</CardDescription>
           </CardHeader>
-          <Separator />
-          <CardContent>
-            <h2 className="text-2xl text-primary">REPORT HISTORY</h2>
-            <ul>
-              {approvedReports.map((report) => (
-                <li key={report.id}>
-                  <WarningIcon aria-hidden className="size-4" /> {report.reason}
-                </li>
-              ))}
-            </ul>
-            <CardDescription>
-              No active reports found for <span className="italic">&quot;{embarkId}&quot;</span>
-            </CardDescription>
-          </CardContent>
-          <CardFooter className="flex gap-2 justify-center items-center">
-            <Button
-              variant="outline"
-              size="default"
-              nativeButton={false}
-              render={
-                <Link to="/" aria-label="Return to search">
-                  <ArrowLeftIcon weight="bold" aria-hidden /> RETURN
-                </Link>
-              }
-            />
-            <ReportDialog embarkId={embarkId} />
-          </CardFooter>
-        </div>
-      </Card>
-    </section>
+        </Card>
+      </section>
+    </>
   );
 }

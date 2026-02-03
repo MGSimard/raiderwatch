f9f5b76c10747e1dccbd1c1777e3e8177e28d867
MGSimard <mgsimard.dev@gmail.com>
2026-02-03 17:25:23 -0500

    Implement upstash redis for session storage and ratelimit (need server function ratelimiting next)

--- src/_auth/index.ts
+++ src/_auth/index.ts
@@ -1,9 +1,10 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "@/_server/db";
+import { redis } from "@/_server/redis";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { admin as adminPlugin } from "better-auth/plugins";
 import { ac, admin, moderator, user } from "./permission";
-import { db } from "@/_server/db";
 import * as schema from "@/_server/db/schema";
 
 export const auth = betterAuth({
@@ -13,6 +14,25 @@ export const auth = betterAuth({
     transaction: true,
     usePlural: true,
   }),
+  secondaryStorage: {
+    get: async (key) => {
+      return await redis.get(key);
+    },
+    set: async (key, value, ttl) => {
+      if (ttl) await redis.set(key, value, { ex: ttl });
+      else await redis.set(key, value);
+    },
+    delete: async (key) => {
+      await redis.del(key);
+    },
+  },
+  rateLimit: {
+    enabled: true,
+    window: 120, // Default
+    max: 5, // Default
+    storage: "secondary-storage", // Upstash Redis
+    modelName: "better-auth-rate-limit", // Default
+  },
   emailAndPassword: {
     enabled: true,
     disableSignUp: true,
